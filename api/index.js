ae9b79880ad9af99fa140bad1d6a6266a5534756
chanducoding001 <chandujobsapplications@gmail.com>
2024-01-20 12:35:21 +0530

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('connected to database');
+}).catch(err=>{
+    console.log(err);
+})
 const app = express();
 
 app.listen(3000,()=>{
