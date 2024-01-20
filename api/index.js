4110c41756a6d1af9c6925d5a03d00e678dc47e3
chanducoding001 <chandujobsapplications@gmail.com>
2024-01-20 12:54:02 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('../api/routes/user.route');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log('connected to database');
@@ -12,3 +13,5 @@ const app = express();
 app.listen(3000,()=>{
     console.log('server connected at port 3000!');
 });
+
+app.use('/api/user',userRoutes);
