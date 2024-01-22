71be09b51f78711724cc5994e167b5c7172a6791
chanducoding001 <chandujobsapplications@gmail.com>
2024-01-22 20:02:14 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./pages/FooterCom";
+import FooterCom from "./pages/FooterCom";
 const App = () => {
   return (
     <BrowserRouter>
@@ -18,6 +20,7 @@ const App = () => {
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/projects" element={<Projects/>}/>
     </Routes>
+    <FooterCom/>
     </BrowserRouter>
   )
 }
